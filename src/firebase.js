cb39ec5fdab8ad8f63d88afe75bb09ea6d7e09cb
Jisu Choi <zzoizisoo@gmail.com>
2021-10-04 15:18:58 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyArD_DnErjRc9LPattYNuz5rU8ncje8bVo',
+	authDomain: 'nwitter-404b1.firebaseapp.com',
+	projectId: 'nwitter-404b1',
+	storageBucket: 'nwitter-404b1.appspot.com',
+	messagingSenderId: '67882743627',
+	appId: '1:67882743627:web:5ffac3ca1fdbc4b0d5dace'
+};
+
+export default firebase.initializeApp(firebaseConfig);
